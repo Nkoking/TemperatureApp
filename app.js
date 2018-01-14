c48d706e9ee9160b77fe3cc3ab6aebdac54c0671
Nkoking <nkonordlund@gmail.com>
2018-01-14 14:57:38 +0200

    Added front-end and started working on graphs

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const temperature = require("./temperatureSchema");
 
-mongoose.connect("mongodb://localhost/tempData", { useMongoClient: true });
+var port = process.env.PORT || 80
+
+server.listen(port, function(){
+    console.log("Server & Api on port: %s", port)
+});
+
+mongoose.connect("mongodb://localhost/tempData", { useMongoClient: true }).then(() => {
+    temparature.find({}, function(err, data){
+        console.log(data)
+       });
+    });
+
 mongoose.Promise = global.Promise;
 
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res){
-    res.send("Use /get and /post to get or post new data.")
+    res.sendFile("index.html");
 });
 
 app.get("/get", function(req, res){
@@ -26,7 +40,7 @@ app.post("/post", function(req, res){
 
     var newTemperature = new temperature();
     newTemperature["temperature"] = req.body.temperature;
-
+    console.log(newTemperature)
     newTemperature.save(function(err){
         if(err){
             res.send(err)};
@@ -34,5 +48,3 @@ app.post("/post", function(req, res){
 
     });
 });
-
-app.listen("80")
---
+++ public/js/main.js
@@ -0,0 +1,77 @@
+//Credit to Aleksi Lassila
+class Navbar extends React.Component {
+  render(){
+    return(
+      <div id="Menu" className="bg">
+        <h1 id="Menu-Header">Temperature App</h1>
+        <div id="MenuIcon" className="holdable">
+          <div id="Navigation" className="holdable">
+          <h3 id="Nav-Label">Navigation</h3>
+            <ul id="Navigation-List">
+              <li className="Navigation-Item-Box holdable"><a className="Navigation-Item Navigation-Item-First" id="Sensor1" href="#">Device</a></li>
+              <li className="line holdable"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const getTemperature = async () => {
+  const temperature = await fetch(`/get`);
+  return await temperature.json();
+};
+
+class Chart extends React.Component {
+  constructor(){
+    super();
+    this.state = {temperature: "fetching..."};
+  }
+  
+  async componentWillMount() {
+
+    var data = {
+      x: [],
+      y: [],
+      type: "scatter"
+    };
+
+    const response = await getTemperature()
+
+    response.forEach(element => {
+      data.x.push(element["date"]);
+      data.y.push(element["temperature"]);
+    });
+
+    console.log(data.y[data.y.length - 1]);
+    this.setState({temperature: data.y[data.y.length - 1]});
+    Plotly.newPlot("graphDiv", data)
+};
+
+render(){
+  return(
+    <div id="Data" className="Data">
+        <h2 className="Data-Heading">Temperature: +{this.state.temperature}°C</h2>
+        <div id="graphDiv"></div>
+    </div>
+  )};
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Chart/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
